e20c02979298889b96968ea0a726ef8edec62912
milan2000savani <80159584+milan2000savani@users.noreply.github.com>
2021-03-06 18:34:49 +0530

    Add files via upload

---
+++ FixedJoyStick.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class FixedJoyStick : MonoBehaviour, IDragHandler, IPointerDownHandler, IPointerUpHandler
+{
+
+    public RectTransform Pad;
+    public Transform Player;
+
+    public Vector3 move;
+    public float moveSpeed;
+public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = eventData.position;
+        transform.localPosition = Vector2.ClampMagnitude(eventData.position - (Vector2)Pad.position, Pad.rect.width * .5f);
+
+        move = new Vector3(transform.localPosition.x, 0, transform.localPosition.y).normalized;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Pad.transform.position = Input.mousePosition;
+
+    }
+
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        transform.localPosition = Vector2.zero;
+        move = Vector3.zero;
+    }
+
+    IEnumerator PlayerMove()
+    {
+        while(true)
+        {
+
+            Player.Translate(move.normalized * moveSpeed * Time.deltaTime, Space.World);
+
+            if(move != Vector3.zero)
+            {
+                Player.rotation = Quaternion.Slerp(Player.rotation, Quaternion.LookRotation(move), 5 * Time.deltaTime);
+            }
+
+            yield return null;
+        }
+    }
+}
---
+++ FloatingJoyStick.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class FloatingJoyStick : MonoBehaviour, IDragHandler, IPointerDownHandler, IPointerUpHandler
+{
+
+    public GameObject joyStick;
+    public GameObject joyStickBG;
+    public Vector2 joyStickVec;
+    public Vector3 move;
+    private Vector2 joyStickTouchPos;
+    private Vector2 joyStickOriginalPos;
+    private float joyStickRadius;
+
+
+    private void Start()
+    {
+        joyStickOriginalPos = joyStickBG.transform.position;
+        joyStickRadius = joyStickBG.GetComponent<RectTransform>().sizeDelta.y-joyStick.GetComponent<RectTransform>().sizeDelta.y/2;
+        
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector2 dragPos = eventData.position;
+        
+        joyStickVec = (dragPos - joyStickTouchPos).normalized;
+        move = new Vector3(joyStickVec.x, 0, joyStickVec.y);
+        float joyStickDist = Vector2.Distance(dragPos, joyStickTouchPos);
+
+        if(joyStickDist < joyStickRadius)
+        {
+            joyStick.transform.position = joyStickTouchPos + joyStickVec * joyStickDist;
+        }
+        else
+        {
+            joyStick.transform.position = joyStickTouchPos + joyStickVec * joyStickRadius; 
+        }
+        
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        joyStick.transform.position = Input.mousePosition;
+        joyStickBG.transform.position = Input.mousePosition;
+        joyStickTouchPos = Input.mousePosition;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        joyStickVec = Vector2.zero;
+        move = Vector3.zero;
+        joyStickBG.transform.position = joyStickOriginalPos;
+        joyStick.transform.position = joyStickBG.transform.position;
+    }
+}
