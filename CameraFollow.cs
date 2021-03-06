170db7f87feccd9fc30b5b1f713dda818b7279b2
milan2000savani <80159584+milan2000savani@users.noreply.github.com>
2021-03-06 18:35:22 +0530

    Add files via upload

---
+++ CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public GameObject Player;
+    Vector3 offSet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offSet = Player.transform.position - transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = (Player.transform.position - offSet);
+    }
+}
